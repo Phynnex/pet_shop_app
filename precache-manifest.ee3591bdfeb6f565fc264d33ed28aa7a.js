9c37126009e18c2ec0fddb61c036f2684b379aa6
Phynnex <angalafynetonye@gmail.com>
2020-08-03 16:57:12 +0100

    Updates

---
+++ precache-manifest.ee3591bdfeb6f565fc264d33ed28aa7a.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e45ea836acb84bb315b1c94625ff00",
+    "url": "/Pet%20shop/index.html"
+  },
+  {
+    "revision": "1734f13bb4168f828eb7",
+    "url": "/Pet%20shop/static/css/2.5486e482.chunk.css"
+  },
+  {
+    "revision": "9601101c7acc2ca8423b",
+    "url": "/Pet%20shop/static/css/main.103d9b4d.chunk.css"
+  },
+  {
+    "revision": "1734f13bb4168f828eb7",
+    "url": "/Pet%20shop/static/js/2.a9904a32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pet%20shop/static/js/2.a9904a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9601101c7acc2ca8423b",
+    "url": "/Pet%20shop/static/js/main.75752fc4.chunk.js"
+  },
+  {
+    "revision": "57a090f1827441aa1860",
+    "url": "/Pet%20shop/static/js/runtime-main.c3b875e2.js"
+  },
+  {
+    "revision": "5958d8063d9c539a32719e05a1961fc2",
+    "url": "/Pet%20shop/static/media/about-1.5958d806.jpg"
+  },
+  {
+    "revision": "597a629182db76feb146dabf13ef3aa0",
+    "url": "/Pet%20shop/static/media/bg_1.597a6291.jpg"
+  },
+  {
+    "revision": "079d49b8ab5fc4bbc5d4425f98c97fc5",
+    "url": "/Pet%20shop/static/media/bg_2.079d49b8.jpg"
+  },
+  {
+    "revision": "d9d09fb437dc31c1635302a0b00c0b5c",
+    "url": "/Pet%20shop/static/media/bg_3.d9d09fb4.jpg"
+  },
+  {
+    "revision": "e509eac7f0d9ad926df64ed86fb0ebd3",
+    "url": "/Pet%20shop/static/media/faq-img.e509eac7.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Pet%20shop/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Pet%20shop/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Pet%20shop/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Pet%20shop/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Pet%20shop/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "99fcb86b192980f5d2c09e70e712f709",
+    "url": "/Pet%20shop/static/media/gallery-1.99fcb86b.jpg"
+  },
+  {
+    "revision": "42794abb1370ddf44264ed50b2964cdd",
+    "url": "/Pet%20shop/static/media/gallery-2.42794abb.jpg"
+  },
+  {
+    "revision": "b0e6a7d19ca6452953536059f83b0f3a",
+    "url": "/Pet%20shop/static/media/gallery-3.b0e6a7d1.jpg"
+  },
+  {
+    "revision": "fcbf9cd6ee3f5ca58f51a58d5b490b95",
+    "url": "/Pet%20shop/static/media/gallery-4.fcbf9cd6.jpg"
+  },
+  {
+    "revision": "a01d6088e3874383ed95996d28545310",
+    "url": "/Pet%20shop/static/media/gallery-5.a01d6088.jpg"
+  },
+  {
+    "revision": "fcac38bb61788396c2ae09c454eaa3f4",
+    "url": "/Pet%20shop/static/media/gallery-6.fcac38bb.jpg"
+  },
+  {
+    "revision": "9567bcc389911fd6f94118a52506b595",
+    "url": "/Pet%20shop/static/media/image_1.9567bcc3.jpg"
+  },
+  {
+    "revision": "c494b8ffd394058738d4f0d5f8984f49",
+    "url": "/Pet%20shop/static/media/image_2.c494b8ff.jpg"
+  },
+  {
+    "revision": "89fdf5ffc7396bfd2425d395adde8a6f",
+    "url": "/Pet%20shop/static/media/image_3.89fdf5ff.jpg"
+  },
+  {
+    "revision": "bbcd5413ffcc46f7d9eef5fb36340659",
+    "url": "/Pet%20shop/static/media/person_1.bbcd5413.jpg"
+  },
+  {
+    "revision": "2cd3dcd36bb7c61c847ac770c2aff481",
+    "url": "/Pet%20shop/static/media/person_2.2cd3dcd3.jpg"
+  },
+  {
+    "revision": "514eee1b06ddf4a5692f7264d3da79aa",
+    "url": "/Pet%20shop/static/media/person_3.514eee1b.jpg"
+  },
+  {
+    "revision": "28d9178ca84b2a5e4b0c6f57a5a34a35",
+    "url": "/Pet%20shop/static/media/pricing-1.28d9178c.jpg"
+  },
+  {
+    "revision": "7a8f1d5294f051b8d8c877669055df39",
+    "url": "/Pet%20shop/static/media/pricing-2.7a8f1d52.jpg"
+  },
+  {
+    "revision": "2bed7f66684bc614f3212126d57e05a5",
+    "url": "/Pet%20shop/static/media/pricing-3.2bed7f66.jpg"
+  }
+]);
