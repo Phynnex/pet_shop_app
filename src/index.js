861125068bdc94a542542b25f97f19f46c41a5c1
Phynnex <angalafynetonye@gmail.com>
2020-08-04 17:15:09 +0100

    updated wit react_router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./styles/styles.css";
 import 'font-awesome/css/font-awesome.min.css';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 // import Script from "./components/script";
 
 
